059ddd66cfb2e3cff791de55fc6a74b4c3135034
Cristina Lugo <cmlugoce@syr.edu>
2018-06-20 14:11:36 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'open-uri'
 require 'pry'
-
+require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+   doc = Nokogiri::HTML(open("./fixtures/student-site/index.html")) 
+   binding.pry 
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,3 +14,4 @@ class Scraper
 
 end
 
+Scraper.scrape_index_page("./fixtures/student-site/index.html")
